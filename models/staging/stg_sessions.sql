7b5c706e88dc770b2f727902fa2a7f0a909a1efb
Liliya Jeromin <zl108108@gmail.com>
2025-07-24 12:28:01 +0200

    staging

---
+++ models/staging/stg_sessions.sql
@@ -0,0 +1,51 @@
+with 
+    source as (
+
+        select * from {{ source('bykeleasing', 'sessions') }} 
+        
+
+    )
+
+    , mapping_data as (
+
+        select * from {{ source('bykeleasing', 'mapping_channel') }}
+
+    )
+
+    , joined as (
+
+        select * except(channel_code)
+        from source
+        join mapping_data
+        on source.channel_code = mapping_data.channel_code
+
+    )
+
+    , transformed as (
+        select 
+
+            session_id,
+            user_id,
+            session_date,
+            session_timestamp,
+            channel_type,
+            channel,
+            costs,
+            leasing_contract_id,
+            device,
+
+            -- user visits sequence
+            row_number() over(
+            partition by user_id
+            order by session_date, user_id
+            ) user_visits_seq,
+
+            --contracts count
+            case when leasing_contract_id is not null then 1 else 0 end contracts 
+
+ from joined
+
+    )
+
+    select * from transformed
+
